d2ae67d6691f5439bd4e25d1cd8ea23c8fed5232
shgamingcorner <shgamingcorner@gmail.com>
2026-04-29 10:41:50 +0800

    functions added

--- Lab2.py
+++ Lab2.py
@@ -12,5 +12,44 @@ def calculate_bmi(height,weight):
         print("Overweight")
 
 
+def display_main_menu():
+    print("Enter numbers seperated with commas eg. '3, 4, 5.2, 6' ")
 
-calculate_bmi(weight=57,height=1.73)
+
+
+def get_user_input():
+    stats = input()
+    sheets = stats.split(", ")
+    listing = [sheets]
+    return listing
+
+
+
+def calc_average(list):
+    noinfo = len(list)
+    for i in range(noinfo):
+        total = total + list[noinfo]
+    
+    average = total / len(list)
+    return average
+
+
+def find_min_max(list)
+    list.sort(key=myFunc)
+    minmaxlist = [list[0], list[-1]]
+    return minmaxlist
+
+
+def sort_temperature()
+    print("Sorting the temperature")
+
+
+def calc_median_temperature()
+    print("calc the temp")
+
+
+
+
+
+
+
