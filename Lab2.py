a5c6cb423f2a2f191f872c2aa98e7496cce71dcf
shgamingcorner <shgamingcorner@gmail.com>
2026-04-29 10:12:02 +0800

    BMI CALC ADDED

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,16 @@
-print("ET0735 (DEVOPS FOR AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height,weight):
+    BMI = weight / (height * height)
+    print("Height=" + str(height))
+    print("Weight=" + str(weight))
+    print("BMI=" + str(BMI))
+
+    if BMI <18.5: 
+        print("Underweight")
+    elif BMI >= 18.5 and BMI  <= 25.0:
+        print("Normal Weight")
+    else:
+        print("Overweight")
+
+
+
+calculate_bmi(weight=57,height=1.73)
